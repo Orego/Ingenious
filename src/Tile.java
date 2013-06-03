c2be8102bcffb797388da46281868c1608708904
Peter Drake <drake@lclark.edu>
2013-06-03 11:37:27 -0700

    Added main() method to Tile.

--- src/Tile.java
+++ src/Tile.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 /** A tile with two colors. */
 public class Tile {
 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a number for the first color: ");
+		int a = in.nextInt();
+		System.out.print("Enter a number for the second color: ");
+		int b = in.nextInt();
+		Tile t = new Tile(a, b);
+		System.out.println("The first number was: " + t.getA());
+		System.out.println("The second number was: " + t.getB());
+	}
+	
 	/** One of the two colors on the tile. */
 	private int a;
 	
