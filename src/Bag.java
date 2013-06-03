bfc7cad282761c7c32885c4e192b09b0a5423482
Sam Levenick <slevenick@lclark.edu>
2013-06-03 16:27:14 -0700

    Added bag class and tests

---
+++ src/Bag.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
+
+public class Bag extends ArrayList<Tile>{
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	}
+	
+	
+	
+	public Bag() {
+		for(int x=0; x<6;  x++){
+			for(int y=0; y<5; y++){
+				add(new Tile(x,x));
+			}
+		}
+		
+		for(int x=0; x<6; x++){
+			for(int y= x+1; y<6; y++){
+				for(int z=0; z<6; z++){
+					add(new Tile(x,y));
+					
+				}
+			}
+				
+		}
+	}
+	
+	
+
+public void shuffle(){
+	int currentSize = size();
+	ArrayList<Tile> temp=new ArrayList(currentSize);
+	for(int x=0; x < currentSize; x++) {
+		temp.add(remove((int)((Math.random())*currentSize)));
+	}
+	addAll(temp);
+}
+	
+}
