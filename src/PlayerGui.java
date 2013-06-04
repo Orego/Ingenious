ef74c601438428022a54a7b38b30b90e52105119
Yung-Pin Chen <ychen@lclark.edu>
2013-06-04 16:32:05 -0700

    Draw a filled black rectangle.

--- src/PlayerGui.java
+++ src/PlayerGui.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.geom.*;
 
 public class PlayerGui {
 
@@ -22,8 +23,17 @@ class SimpleFrame extends JFrame
 	public SimpleFrame()
 	{
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		add(new PlayerComponent());
 	}
 	
 	public static final int DEFAULT_WIDTH = 300;
 	public static final int DEFAULT_HEIGHT = 200;
+}
+
+class PlayerComponent extends JComponent {
+	
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.fill(new Rectangle2D.Double(100, 100, 100, 75));
+	}
 }
