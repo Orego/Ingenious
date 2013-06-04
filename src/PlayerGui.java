44d4f62edce3fe86c9632bc75332786e73447a97
Yung-Pin Chen <ychen@lclark.edu>
2013-06-04 16:18:09 -0700

    Display a graphic window.

--- src/PlayerGui.java
+++ src/PlayerGui.java
@@ -1,4 +1,29 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class PlayerGui {
 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				SimpleFrame frame = new SimpleFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+
+			}
+		});
+
+	}
+
+}
+
+class SimpleFrame extends JFrame
+{
+	public SimpleFrame()
+	{
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+	
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
 }
