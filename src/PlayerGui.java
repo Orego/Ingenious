4bab051a23f6ccaf974878f5441c50274d2198b1
Yung-Pin Chen <ychen@lclark.edu>
2013-06-04 18:11:02 -0700

    Modify (decrease) the tile size and score board size.

--- src/PlayerGui.java
+++ src/PlayerGui.java
@@ -4,18 +4,19 @@ import java.awt.geom.*;
 
 public class PlayerGui {
 
+	/**
+	 * Drawing a window frame. Part of the code is taken from Core Java, Volume
+	 * 1, page 286
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				SimpleFrame frame = new SimpleFrame();
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
-
 			}
 		});
-
 	}
-
 }
 
 class SimpleFrame extends JFrame {
@@ -24,20 +25,19 @@ class SimpleFrame extends JFrame {
 		add(new PlayerComponent());
 	}
 
-	public static final int DEFAULT_WIDTH = 600;
-	public static final int DEFAULT_HEIGHT = 800;
+	public static final int DEFAULT_WIDTH = 230;
+	public static final int DEFAULT_HEIGHT = 530;
 }
 
 class PlayerComponent extends JComponent {
-
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		
+
 		// Draw the six tiles of a player's hand
 		for (int i = 0; i < 6; i++) {
-			g2.fill(new Rectangle2D.Double(40, 40 + i * 125, 360, 85));
+			g2.fill(new Rectangle2D.Double(30, 30 + i * 80, 100, 50));
 		}
-		
+
 		// Display the scores of a player's hand
 		for (int i = 0; i < 6; i++) {
 			if (i == 0) {
@@ -53,8 +53,8 @@ class PlayerComponent extends JComponent {
 			} else {
 				g2.setPaint(Color.MAGENTA);
 			}
-			
-			g2.fill(new Rectangle2D.Double(440, 40 + i * 125, 120, 85));
+
+			g2.fill(new Rectangle2D.Double(160, 30 + i * 80, 50, 50));
 		}
 	}
 }
