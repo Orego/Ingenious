84594293a0b050bce4f7c3c5acd8ec37a0ee9a24
Brandon Cieslak <bcieslak@lclark.edu>
2013-06-05 13:19:46 -0700

    The player's hand adds tiles from the bag until it has 6 tiles after the player places a tile. Also gave each new game its own bag. Players are now given the game they are playing as a constructor argument so that they may reference it. The player's hand gets the game's bag. Finally, the player places tiles through the placeTile() method in the Player class, which has the added functionality of removing that tile from the hand and getting a new tile out of the bag.

--- src/Player.java
+++ src/Player.java
@@ -2,15 +2,24 @@ import java.util.Arrays;
 
 public class Player {
 	private int[] score;
+	private Hand hand;
 	private boolean hasPlayed;
-
-	public Player() {
+	private Game gameBeingPlayed;
+	
+	public Player(Game game) {
 		score = new int[6];
+		hand = new Hand(game.getBag());
 		hasPlayed = false;
+		gameBeingPlayed = game;
+	}
+	
+	public Player() {
+		this(null);
 	}
 
 	public static void main(String[] args) {
-		Player p = new Player();
+		Game g = new Game();
+		Player p = g.getPlayer(0);
 		System.out.println("Your initial scores are:\n"
 				+ Arrays.toString(p.score));
 		p.addScore(2, 4);
@@ -18,8 +27,17 @@ public class Player {
 		System.out
 				.println("After adding 4 points to color 2 and adding 1 point to color 5, your scores are:\n"
 						+ Arrays.toString(p.score));
+		System.out.println("\nA random hand:\n" + p.getHand());
+		p.placeTile(5, 0, 1, 0);
+		System.out.println("After playing the 6th tile in their hand, their new hand is:\n" + p.getHand());
+		
 	}
 
+	/** Gets the hand of the player. */
+	public Hand getHand() {
+		return hand;
+	}
+	
 	/** Gets the score of given color */
 	public int getScore(int color) {
 		return score[color];
@@ -35,8 +53,23 @@ public class Player {
 		return hasPlayed;
 	}
 	
+	/** Places a tile from the hand and removes that tile from their hand */
+	public void placeTile(int tileIndex, int row, int column, int rotation) {
+		Tile tile = hand.get(tileIndex);
+		gameBeingPlayed.placeTile(tile, row, column, rotation);
+		hand.remove(tileIndex);
+		refreshHand();
+	}
+	
 	/** This should be called to indicate that this player has played their first turn already. */
 	public void played() {
 		hasPlayed = true;
+	}
+	
+	/** Fills the hand to six tiles. */
+	public void refreshHand() {
+		while(hand.size() < 6) {
+			hand.add(gameBeingPlayed.getBag().draw());
+		}
 	}
 }
--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,18 +1,34 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerTest {
-
+	Game g;
+	Player p0;
+	
+	@Before
+	public void setUp() throws Exception {
+		g = new Game();
+		p0 = g.getPlayer(0);
+	}
+	
 	@Test
 	public void testGetScore() {
-		Player p = new Player();
-		assertEquals(0, p.getScore(2));
-		p.addScore(2, 2);
-		assertEquals(2, p.getScore(2));
-		p.addScore(2, 3);
-		assertEquals(5, p.getScore(2));
-		assertEquals(0, p.getScore(3));
+		assertEquals(0, p0.getScore(2));
+		p0.addScore(2, 2);
+		assertEquals(2, p0.getScore(2));
+		p0.addScore(2, 3);
+		assertEquals(5, p0.getScore(2));
+		assertEquals(0, p0.getScore(3));
 	}
 
+	@Test
+	public void testRefreshHand() {
+		Hand oldHand = new Hand();
+		oldHand.addAll(p0.getHand());
+		p0.placeTile(5, 0, 1, 0);
+		assertEquals(6, p0.getHand().size());
+		assertTrue(oldHand.get(5) != p0.getHand().get(5));
+	}
 }
