21b34233b547a5005abc596405489b315be333d3
Samuel Kelly <samkelly@lclark.edu>
2013-06-05 16:23:12 -0700

    User can move and place a default tile on the board, using the mouse.

--- src/DrawBoard.java
+++ src/DrawBoard.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+//import java.awt.event.MouseListener;
+//import java.awt.event.MouseMotionListener;
+
+import javax.swing.event.MouseInputAdapter;
 import java.util.Scanner;
 
 public class DrawBoard {
 	
-	public static class GameInfo {
-		public Game game = new Game();
-	}
+	private static final UIState uiState = new UIState();
 	
 	public static void main(String[] args) {
 		
@@ -16,8 +19,7 @@ public class DrawBoard {
 		int rotation;
 		Tile tile = new Tile((int)(Math.random()*6), (int)(Math.random()*6));
 		
-		final Game game = new Game();
-		final BoardFrame bf = new BoardFrame(game);
+		final BoardFrame bf = new BoardFrame(uiState);
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -27,59 +29,62 @@ public class DrawBoard {
 			}
 		});
 		
-		while (true) {
-			System.out.println(game.getBoard() + "\n\n Current Tile is " + tile
-					+ "\nPlayer " + game.getCurrentPlayerIndex() + " give rotation of tile:\n" +
-							"(Color 2 rotates around Color 1. E is 0, NE is 1, NW is 2, etc..)");
-			rotation = in.nextInt();
-			System.out.println("Give row to place tile:");
-			row = in.nextInt();
-			System.out.println("Give column to place tile:");
-			column = in.nextInt();
-			if(game.placeTile(tile, row, column, rotation)) {
-				tile = new Tile((int)(Math.random()*6), (int)(Math.random()*6));
-			} else {
-				System.out.println("Invalid move: try again");
-			}
-			bf.repaint();
-		}
+//		while (true) {
+//			System.out.println(uiState.game.getBoard() + "\n\n Current Tile is " + tile
+//					+ "\nPlayer " + uiState.game.getCurrentPlayerIndex() + " give rotation of tile:\n" +
+//							"(Color 2 rotates around Color 1. E is 0, NE is 1, NW is 2, etc..)");
+//			rotation = in.nextInt();
+//			System.out.println("Give row to place tile:");
+//			row = in.nextInt();
+//			System.out.println("Give column to place tile:");
+//			column = in.nextInt();
+//			if(uiState.game.placeTile(tile, row, column, rotation)) {
+//				tile = new Tile((int)(Math.random()*6), (int)(Math.random()*6));
+//			} else {
+//				System.out.println("Invalid move: try again");
+//			}
+		bf.repaint();
+//		}
 
 	}	
 }
 
 class BoardFrame extends JFrame {
-	public BoardFrame(Game game) {
+	UIState uiState;
+	public BoardFrame(UIState uiState) {
 		setTitle("Ingenious");
-		BoardComponent comp = new BoardComponent(game);
-		setSize((int) (40 + 11 * BoardComponent.HEX_WIDTH),
-				(int) (60 + BoardComponent.HEX_HEIGHT + 0.75 * BoardComponent.HEX_HEIGHT * 10));
+		this.uiState = uiState;
+		BoardComponent comp = new BoardComponent(uiState);
+		setSize((int) (40 + 11 * UIState.HEX_WIDTH) + 300,
+				(int) (60 + UIState.HEX_HEIGHT + 0.75 * UIState.HEX_HEIGHT * 10));
 		add(comp);
 	}
 }
 
-class BoardComponent extends JComponent {
-	Game game;
-	public static final int HEX_HEIGHT = 50;
-	public static final double HEX_WIDTH = HEX_HEIGHT * Math.sqrt(3)/2;
-	public BoardComponent(Game game) {
-		this.game = game;
+class BoardComponent extends JComponent implements MouseListener, MouseMotionListener {
+	UIState uiState;
+	public BoardComponent(UIState uiState) {
+		this.uiState = uiState;
+		addMouseListener(this);
+		addMouseMotionListener(this);
 	}
-	public void paintComponent(Graphics g) {
+	
+	public void drawBoard(Graphics g) {
 		Polygon[][] hex = new Polygon[11][11];
 		double angle;
 		double centerx;
 		double centery;
-		Board board = game.getBoard();
+		Board board = uiState.game.getBoard();
 		for(int i = 0; i < 11; i++) {
 			for(int j = 0; j < 11; j++) {
 				hex[i][j] = new Polygon();
 				if(board.isValidHex(i, j)) {
-					centerx = 40 + 0.5 * HEX_WIDTH * (5 - i) + HEX_WIDTH * j;
-					centery = 40 + 0.75*HEX_HEIGHT*i;
+					centerx = 40 + 0.5 * UIState.HEX_WIDTH * (5 - i) + UIState.HEX_WIDTH * j;
+					centery = 40 + 0.75*UIState.HEX_HEIGHT*i;
 					for(int k = 0; k < 6; k++) {			
 						angle = 2 * Math.PI/6 * (k);
-						hex[i][j].addPoint((int)(centerx + (HEX_HEIGHT)/2 * Math.sin(angle)), 
-								(int)(centery + (HEX_HEIGHT)/2 * Math.cos(angle)));
+						hex[i][j].addPoint((int)(centerx + (UIState.HEX_HEIGHT)/2 * Math.sin(angle)), 
+								(int)(centery + (UIState.HEX_HEIGHT)/2 * Math.cos(angle)));
 					}
 					switch (board.getHex(i, j).getColor()) {
 					case 0:
@@ -120,5 +125,105 @@ class BoardComponent extends JComponent {
 				}
 			}
 		}
+	}
+	
+	public void drawTile(Graphics g) {
+		
+		if (!uiState.validTilePosition) {
+			return;
+		}
+		
+		Polygon[] hexes = { new Polygon(), new Polygon() };
+
+		// if rotation, change angle to 60deg * rotation
+		
+		double x = uiState.centerx;
+		double y = uiState.centery;
+		
+		for (int i = 0; i < 2; i++) {
+			x = x + i * UIState.HEX_WIDTH * Math.cos(Math.PI / 3 * 2);
+			y = y - i * UIState.HEX_WIDTH * Math.sin(Math.PI / 3 * 2);
+			
+			for(int j = 0; j < 6; j++) {			
+				double angle = 2 * Math.PI/6 * j;
+				hexes[i].addPoint((int)(x + UIState.HEX_HEIGHT/2.0 * Math.sin(angle)), 
+						(int)(y + UIState.HEX_HEIGHT/2.0 * Math.cos(angle)));
+			}
+			if(i < 1) {
+				g.setColor(Color.RED);
+			} else{				
+				g.setColor(Color.CYAN);
+			}
+			g.fillPolygon(hexes[i]);
+			g.setColor(Color.BLACK);
+			g.drawPolygon(hexes[i]);
+		}
+	}
+
+	
+	public void paintComponent(Graphics g) {
+		if (uiState.currentState == UIState.PLACE_TILE_OR_RESELECT) {
+			drawBoard(g);
+			drawTile(g);
+			// accept mouseclicks and movements
+		}
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent event) {
+		// find which hexagon the mouse is in (which one's center it is closest to)
+		
+		int row = (int) Math.round(4.0/3.0 * (event.getY() - 40.0) / UIState.HEX_HEIGHT);
+		int col = (int) Math.round((event.getX() - 40 - 0.5 * UIState.HEX_WIDTH * (5 - row)) / UIState.HEX_WIDTH);
+		
+		if (uiState.game.isValidTilePlacement(row, col, 2)) {
+			uiState.validTilePosition = true;
+			uiState.centerx = 40 + 0.5 * UIState.HEX_WIDTH * (5 - row) + UIState.HEX_WIDTH * col;
+			uiState.centery = 40 + 0.75 * UIState.HEX_HEIGHT * row;
+		} else {
+			uiState.validTilePosition = false;
+		}
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent event) {
+		int row = (int) Math.round(4.0/3.0 * (event.getY() - 40.0) / UIState.HEX_HEIGHT);
+		int col = (int) Math.round((event.getX() - 40 - 0.5 * UIState.HEX_WIDTH * (5 - row)) / UIState.HEX_WIDTH);
+		
+		if (uiState.game.isValidTilePlacement(row, col, 2)) {
+			uiState.game.placeTile(new Tile(0, 4), row, col, 2);
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
