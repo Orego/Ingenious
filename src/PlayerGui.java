c40972888f98080660224375e7fc7099d80da2ac
Yung-Pin Chen <ychen@lclark.edu>
2013-06-04 17:02:21 -0700

    Color each of the six score board with its corresponding color.

--- src/PlayerGui.java
+++ src/PlayerGui.java
@@ -18,22 +18,43 @@ public class PlayerGui {
 
 }
 
-class SimpleFrame extends JFrame
-{
-	public SimpleFrame()
-	{
+class SimpleFrame extends JFrame {
+	public SimpleFrame() {
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		add(new PlayerComponent());
 	}
-	
-	public static final int DEFAULT_WIDTH = 300;
-	public static final int DEFAULT_HEIGHT = 200;
+
+	public static final int DEFAULT_WIDTH = 600;
+	public static final int DEFAULT_HEIGHT = 800;
 }
 
 class PlayerComponent extends JComponent {
-	
+
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		g2.fill(new Rectangle2D.Double(100, 100, 100, 75));
+		
+		// Draw the six tiles of a player's hand
+		for (int i = 0; i < 6; i++) {
+			g2.fill(new Rectangle2D.Double(40, 40 + i * 125, 360, 85));
+		}
+		
+		// Display the scores of a player's hand
+		for (int i = 0; i < 6; i++) {
+			if (i == 0) {
+				g2.setPaint(Color.RED);
+			} else if (i == 1) {
+				g2.setPaint(Color.ORANGE);
+			} else if (i == 2) {
+				g2.setPaint(Color.YELLOW);
+			} else if (i == 3) {
+				g2.setPaint(Color.GREEN);
+			} else if (i == 4) {
+				g2.setPaint(Color.BLUE);
+			} else {
+				g2.setPaint(Color.MAGENTA);
+			}
+			
+			g2.fill(new Rectangle2D.Double(440, 40 + i * 125, 120, 85));
+		}
 	}
 }
