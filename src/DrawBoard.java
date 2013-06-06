72adcce321346354d9478d187860c9508eb46fd8
Yung-Pin Chen <ychen@lclark.edu>
2013-06-06 10:02:19 -0700

    Incorporated PlayerGui into DrawBoard.

--- src/DrawBoard.java
+++ src/DrawBoard.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DrawBoard {
@@ -53,8 +55,8 @@ class BoardFrame extends JFrame {
 	public BoardFrame(Game game) {
 		setTitle("Ingenious");
 		BoardComponent comp = new BoardComponent(game);
-		setSize((int) (40 + 11 * BoardComponent.HEX_WIDTH),
-				(int) (60 + BoardComponent.HEX_HEIGHT + 0.75 * BoardComponent.HEX_HEIGHT * 10));
+		setSize((int) (271 + 11 * BoardComponent.HEX_WIDTH),
+				(int) (100 + BoardComponent.HEX_HEIGHT + 0.75 * BoardComponent.HEX_HEIGHT * 10));
 		add(comp);
 	}
 }
@@ -77,14 +79,111 @@ class BoardComponent extends JComponent {
 				drawHex(g, board, i, j);
 			}
 		}
+		drawHand(g);
 	}
+	
+	/** Draws a hexagon around the given center coordinates, with the given color. */
+	public void drawHandHex(Graphics g, int centerx, int centery, int color) {
+		Polygon hex = new Polygon();
+		double angle;
+		for(int k = 0; k < 6; k++) {			
+			angle = 2 * Math.PI/6 * (k);
+			hex.addPoint((int)(centerx + (HEX_HEIGHT)/2 * Math.sin(angle)), 
+					(int)(centery + (HEX_HEIGHT)/2 * Math.cos(angle)));
+			g.setColor(Color.BLACK);
+			g.fillPolygon(hex);
+		}
+		switch (color) {
+		case 0:
+			g.setColor(Color.RED);
+			g.fillOval( (int)(centerx-HEX_HEIGHT/4), (int)(centery-HEX_HEIGHT/4), 2*HEX_HEIGHT/4, 2*HEX_HEIGHT/4);
+			
+			break;
+		case 1:
+			g.setColor(Color.ORANGE);
+			g.fillRect( (int)(centerx-HEX_HEIGHT/4), (int)(centery-HEX_HEIGHT/4), 2*HEX_HEIGHT/4, 2*HEX_HEIGHT/4);
+			break;
+		case 2:
+			
+			g.setColor(Color.YELLOW);
+			g.fillRect( (int)(centerx-HEX_HEIGHT/4), (int)(centery-HEX_HEIGHT/8), 2*HEX_HEIGHT/4, 2*HEX_HEIGHT/8);
+			g.fillRect( (int)(centerx-HEX_HEIGHT/8), (int)(centery-HEX_HEIGHT/4), 2*HEX_HEIGHT/8, 2*HEX_HEIGHT/4);
+			
+			break;
+		case 3:
+			g.setColor(Color.GREEN);
+			double d = HEX_HEIGHT / 4;
+			double h = (int)(d * Math.sqrt(3) / 2);
+			Polygon triangle = new Polygon(new int[] {(int)(centerx - d), (int)(centerx), (int)(centerx + d)}, new int[] {(int)(centery + h), (int)(centery - h), (int)(centery + h)}, 3);
+			g.fillPolygon(triangle);
+			break;
+		case 4:
+			g.setColor(Color.CYAN);
+		    h= HEX_HEIGHT/4;
+			g.fillRect( (int)(centerx-h), (int)(centery-h), (int)(2*h), (int)(2*h/3));
+			g.fillRect( (int)(centerx-h), (int)(centery+h/3), (int)(2*h), (int)(2*h/3));
+			
+			
+			break;
+		case 5:
+			g.setColor(Color.MAGENTA);
+			h = HEX_HEIGHT / 4;
+			Polygon diamond = new Polygon(new int[] {(int)(centerx - h), (int)(centerx), (int)(centerx + h), (int)(centerx)}, new int[] {(int)(centery), (int)(centery - h), (int)(centery), (int)(centery + h)}, 4);
+			g.fillPolygon(diamond);
+			
+			break;
+		default: // vacant
+			
+		}
+	}
+	
+	protected void drawHand(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		int centerx;
+		int centery;
+		// Draw the six tiles of a player's hand
+		for (int i = 0; i < 6; i++) {
+			centerx = (int)(30 + HEX_WIDTH / 2);
+			centery = 30 + HEX_HEIGHT / 2 + 80 * i;
+			drawHandHex(g, centerx, centery, game.getPlayer(game.getCurrentPlayerIndex()).getHand().get(i).getA());
+			centerx += HEX_WIDTH;
+			drawHandHex(g, centerx, centery, game.getPlayer(game.getCurrentPlayerIndex()).getHand().get(i).getB());
+			centerx -= HEX_WIDTH;
+		}
+
+		// Display the scores of a player's hand
+		for (int i = 0; i < 6; i++) {
+			if (i == 0) {
+				g2.setPaint(Color.RED);
+			} else if (i == 1) {
+				g2.setPaint(Color.ORANGE);
+			} else if (i == 2) {
+				g2.setPaint(Color.YELLOW);
+			} else if (i == 3) {
+				g2.setPaint(Color.GREEN);
+			} else if (i == 4) {
+				g2.setPaint(Color.CYAN);
+			} else {
+				g2.setPaint(Color.MAGENTA);
+			}
+
+			g2.fill(new Rectangle2D.Double(160, 30 + i * 80, 50, 50));
+			g2.setPaint(Color.BLACK);
+			int score = game.getPlayer(game.getCurrentPlayerIndex()).getScore(i);
+			String s = score + "";
+			g2.drawString(s, 180, 60 + i * 80);
+		}
+		String playerString = "Player " + (game.getCurrentPlayerIndex() + 1) + "'s turn.";
+		g2.drawString(playerString, (int)(170 + 0.5 * (70 + 11 * HEX_WIDTH)), 475);
+	}
+	
 	protected void drawHex(Graphics g, Board board, int i, int j) {
 		double angle;
 		double centerx;
 		double centery;
 		Polygon hex = new Polygon();
 		if(board.isValidHex(i, j)) {
-			centerx = 40 + 0.5 * HEX_WIDTH * (5 - i) + HEX_WIDTH * j;
+			centerx = 271 + 0.5 * HEX_WIDTH * (5 - i) + HEX_WIDTH * j;
 			centery = 40 + 0.75*HEX_HEIGHT*i;
 			for(int k = 0; k < 6; k++) {			
 				angle = 2 * Math.PI/6 * (k);
