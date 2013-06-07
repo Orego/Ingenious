4576caa2bdd3d246966de775f4e6bde459010533
Peter Drake <drake@lclark.edu>
2013-06-07 14:58:24 -0700

    Moved ButtonActions to its own class.

--- src/DrawBoard.java
+++ src/DrawBoard.java
@@ -335,31 +335,3 @@ class BoardComponent extends HexGui implements MouseListener, MouseMotionListene
 	}
 }
 
-class ButtonActions implements ActionListener {
-	private UIState uiState;
-	private Game game;
-	private boolean refresh;
-	private JPanel panel;
-	public ButtonActions(UIState uiState, int i, JPanel panel){
-		this.uiState = uiState;
-		this.panel=panel;
-		game=uiState.game;
-		if(i==0){
-			refresh=true;
-		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		if(refresh){
-			game.getPlayer(game.getCurrentPlayerIndex()).refreshHand();
-		}else{
-			game.getPlayer(game.getCurrentPlayerIndex()).swapTiles();
-		}
-		panel.getComponent(5).setEnabled(false);
-		panel.getComponent(4).setEnabled(false);
-		panel.repaint();
-		
-	}
-
-}
